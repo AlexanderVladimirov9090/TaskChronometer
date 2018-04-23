d084e0c1ca045e5c6ca5db85dd4093961daa4901
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-04-23 12:08:08 +0300

    Still working on Provider.`

--- app/src/main/java/com/gmail/alexander/taskchronometer/databasetools/AppProvider.java
+++ app/src/main/java/com/gmail/alexander/taskchronometer/databasetools/AppProvider.java
@@ -2,32 +2,90 @@ package com.gmail.alexander.taskchronometer.databasetools;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import javax.xml.datatype.Duration;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by:
  *
  * @author Alexander Vladimirov
- *         <alexandervladimirov1902@gmail.com>
- *
+ * <alexandervladimirov1902@gmail.com>
  */
 
-public class AppProvider extends ContentProvider{
+public class AppProvider extends ContentProvider {
     private AppDatabase openHelper;
+    public static final UriMatcher uriMatcher = buildUriMatcher();
     static final String CONTENT_AUTHORITY = "com.gmail.alexandertaskchronometer";
-    public static final Uri CONTENT_AUTHORITY_URI= Uri.parse("content://"+ CONTENT_AUTHORITY);
+    public static final Uri CONTENT_AUTHORITY_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final int TASKS = 100;
+    private static final int TASKS_ID = 101;
+    private static final int TIMINGS = 200;
+    private static final int TIMINGS_ID = 201;
+
+    /*
+        private static final int TASK_TIMINGS= 300;
+        private static final int TASK_TIMINGS_ID=301;
+    */
+    public static final int TASK_DURATIONS = 400;
+    public static final int TASK_DURATIONS_ID = 401;
 
     @Override
     public boolean onCreate() {
-        return false;
+        openHelper = AppDatabase.getInstance(getContext());
+        return true;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        Log.d(TAG, "query: called with URI: "+ uri);
+      final int match = uriMatcher.match(uri);
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        switch (match){
+            case TASKS:
+                queryBuilder.setTables(TaskContract.TABLE_NAME);
+                break;
+            case TASKS_ID:
+                queryBuilder.setTables(TaskContract.TABLE_NAME);
+                long taskId= TaskContract.getTaskId(uri);
+                queryBuilder.appendWhere(TaskContract.Columns._ID+ "="+taskId);
+                break;
+
+            case TIMINGS:
+                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+                break;
+            case TIMINGS_ID:
+                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+                long timingId= TimingContract.getTimingId(uri);
+                queryBuilder.appendWhere(TimingsContract.Columns._ID+ "="+timingId);
+                break;
+
+            case TASK_DURATIONS:
+                queryBuilder.setTables(DurationsContract.TABLE_NAME);
+                break;
+            case TASK_DURATIONS_ID:
+                queryBuilder.setTables(DurationsContract.TABLE_NAME);
+                long durationId= DurationsContract.getDurationId(uri);
+                queryBuilder.appendWhere(DurationsContract.Columns._ID+ "="+durationId);
+                break;
+
+                default:
+                    throw new IllegalArgumentException("Unknown URI: "+ uri);
+        }
+         SQLiteDatabase db= openHelper.getReadableDatabase();
+        Cursor cursor = queryBuilder.query(db,projection,selection,selectionArgs,null,null,sortOrder);
+
         return null;
     }
 
@@ -51,5 +109,18 @@ public class AppProvider extends ContentProvider{
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         return 0;
+    }
+    private static UriMatcher buildUriMatcher(){
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        matcher.addURI(CONTENT_AUTHORITY, TaskContract.TABLE_NAME, TASKS);
+        matcher.addURI(CONTENT_AUTHORITY,TaskContract.TABLE_NAME+ "/#", TASKS_ID);
+
+        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME, TIMINGS);
+        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME+ "/#", TIMINGS_ID);
+
+        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME, TASK_DURATIONS);
+        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME+ "/#", TASK_DURATIONS_ID);
+    return matcher;
     }
 }
