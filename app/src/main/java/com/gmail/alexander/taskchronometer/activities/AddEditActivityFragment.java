0437a059072cfe24c593f9a61ed9f5db8bd5df3a
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-05-03 11:22:16 +0300

    Working on add and edit logic.

--- app/src/main/java/com/gmail/alexander/taskchronometer/activities/AddEditActivityFragment.java
+++ app/src/main/java/com/gmail/alexander/taskchronometer/activities/AddEditActivityFragment.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.gmail.alexander.taskchronometer.R;
+import com.gmail.alexander.taskchronometer.domain_layer.Task;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +19,9 @@ public class AddEditActivityFragment extends Fragment {
     private static final String TAG = "AddEditActivityFragment";
 
     public enum FragmentEditMode {EDIT, ADD}
+
+    private FragmentEditMode mode;
+
     private EditText nameText;
     private EditText descritpionText;
     private EditText sortOrderText;
@@ -30,12 +34,31 @@ public class AddEditActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: Stats");
-        View view = inflater.inflate(R.layout.fragment_add_edit,container,false);
+        View view = inflater.inflate(R.layout.fragment_add_edit, container, false);
         nameText = (EditText) view.findViewById(R.id.addedit_name);
         descritpionText = (EditText) view.findViewById(R.id.addedit_description);
-        sortOrderText =  (EditText) view.findViewById(R.id.addedit_sortorder);
+        sortOrderText = (EditText) view.findViewById(R.id.addedit_sortorder);
         saveButton = (Button) view.findViewById(R.id.addedit_save);
+        Bundle argument = getActivity().getIntent().getExtras();
+        final Task task;
+        if (argument != null) {
+            Log.d(TAG, "onCreateView: Retrieving task details");
+            task = (Task) argument.getSerializable(Task.class.getSimpleName());
+            if (task != null) {
+                Log.d(TAG, "onCreateView: Task details found editing...");
+                nameText.setText(task.getName());
+                descritpionText.setText(task.getDescription());
+                sortOrderText.setText(Integer.toString(task.getSortOrder()));
+                mode = FragmentEditMode.EDIT;
+            }else {
+                mode = FragmentEditMode.ADD;
 
-        return inflater.inflate(R.layout.fragment_add_edit, container, false);
+            }
+        }else {
+            task = null;
+            Log.d(TAG, "onCreateView: No arguments, adding new record.");
+            mode = FragmentEditMode.ADD;
+        }
+        return view;
     }
 }
