508b7695e5e1a19900250335c8a423b18da81941
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-05-04 13:00:26 +0300

    Implementing LoaderCallbarcks.

--- app/src/main/java/com/gmail/alexander/taskchronometer/MainActivityFragment.java
+++ app/src/main/java/com/gmail/alexander/taskchronometer/MainActivityFragment.java
@@ -1,7 +1,11 @@
 package com.gmail.alexander.taskchronometer;
 
-import android.support.v4.app.Fragment;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +13,9 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "MainActivityFragment";
+    public static final int LOADER_ID = 0;
 
     public MainActivityFragment() {
     }
@@ -17,6 +23,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: Starts");
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
