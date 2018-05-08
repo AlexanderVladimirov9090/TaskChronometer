1915ec77198666544e3c29f7c6452cd35887820c
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-05-08 10:18:36 +0300

    Working on CursorRecyclerView.

---
+++ app/src/main/java/com/gmail/alexander/taskchronometer/adapters/CursorRecyclerView.java
@@ -0,0 +1,28 @@
+package com.gmail.alexander.taskchronometer.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by:
+ *
+ * @author Alexander Vladimirov
+ * <alexandervladimirov1902@gmail.com>
+ */
+public class CursorRecyclerView extends RecyclerView.Adapter<TaskViewHolder>{
+
+    @Override
+    public TaskViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(TaskViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/gmail/alexander/taskchronometer/adapters/TaskViewHolder.java
@@ -0,0 +1,10 @@
+package com.gmail.alexander.taskchronometer.adapters;
+
+/**
+ * Created by:
+ *
+ * @author Alexander Vladimirov
+ * <alexandervladimirov1902@gmail.com>
+ */
+class TaskViewHolder {
+}
