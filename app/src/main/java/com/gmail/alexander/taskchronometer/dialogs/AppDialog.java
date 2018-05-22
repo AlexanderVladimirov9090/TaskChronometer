35fad58a62358db6fdc9beb40e275e599b41e79e
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-05-22 12:22:52 +0300

    Working on dialog to the user.

--- app/src/main/java/com/gmail/alexander/taskchronometer/dialogs/AppDialog.java
+++ app/src/main/java/com/gmail/alexander/taskchronometer/dialogs/AppDialog.java
@@ -1,14 +1,22 @@
 package com.gmail.alexander.taskchronometer.dialogs;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.util.Log;
+
+import com.gmail.alexander.taskchronometer.R;
 
 /**
  * Created by:
  *
  * @author Alexander Vladimirov
  * <alexandervladimirov1902@gmail.com>
+ *     This is where Dialog boxes are configurated.
+ *
  */
 public class AppDialog extends DialogFragment {
     private static final String TAG = "AppDialog";
@@ -34,13 +42,58 @@ public class AppDialog extends DialogFragment {
     }
 
     /**
-     * Resets dialogEvents.
+     * Resets dialog events.
      */
     @Override
     public void onDetach() {
         Log.d(TAG, "onDetach: Entering...");
         super.onDetach();
         dialogEvents = null;
+    }
+
+    /**
+     * This is where the creation of Dialog box is created.
+     *
+     * @param savedInstanceState
+     * @return Dialog object.
+     */
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateDialog: Starts");
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Bundle arguments = getArguments();
+        final int dialogId;
+        String messageString;
+        int positiveStringId;
+        int negativeStringId;
+        if (arguments != null) {
+            dialogId = arguments.getInt(DIALOG_ID);
+            messageString = arguments.getString(DIALOG_MESSAGE);
+            positiveStringId = arguments.getInt(DIALOG_POSITIVE_RID);
+            if (positiveStringId == 0) {
+                positiveStringId == R.string.ok;
+            }
+            negativeStringId = arguments.getInt(DIALOG_NEGATIVE_RID);
+            if (negativeStringId == 0) {
+                negativeStringId = R.string.cancel;
+            }
+        } else {
+            throw new IllegalArgumentException("Must pass DIALOG_ID and DIALOG_MESSAGE in the bundle.");
+        }
+        builder.setMessage(messageString).setPositiveButton(positiveStringId, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //On positive result this action will take place.
+                dialogEvents.onPositiveDialogResult(dialogId, arguments);
+            }
+        }).setNegativeButton(negativeStringId, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //On negative result this action will take place.
+                dialogEvents.onNegativeDialogResult(dialogId, arguments);
+            }
+        });
+        return super.onCreateDialog(savedInstanceState);
     }
 }
 
