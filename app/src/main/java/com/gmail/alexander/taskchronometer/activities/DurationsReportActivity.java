eba0bae92b1b855f2808097cf866ece81fc7e58d
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2018-06-29 15:19:37 +0300

    Calendar Filter is good to go.

--- app/src/main/java/com/gmail/alexander/taskchronometer/activities/DurationsReportActivity.java
+++ app/src/main/java/com/gmail/alexander/taskchronometer/activities/DurationsReportActivity.java
@@ -1,8 +1,10 @@
 package com.gmail.alexander.taskchronometer.activities;
 
+import android.app.DatePickerDialog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.BaseColumns;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
@@ -14,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
 
 import com.gmail.alexander.taskchronometer.R;
 import com.gmail.alexander.taskchronometer.adapters.DurationsRecViewAdapter;
@@ -24,14 +27,17 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-public class DurationsReportActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class DurationsReportActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, DatePickerDialog.OnDateSetListener {
     public static final String CURRENT_DATE = "CURRENT_DATE";
     public static final String DISPLAY_WEEK = "DISPLAY_WEEK";
 
-    private static final String TAG = "DurationsReportActivity";
+    public static final int DIALOG_FILTER = 1;
+    public static final int DIALOG_DELETE = 2;
+
     private static final int LOADER_ID = 1;
+    private static final String TAG = "DurationsReportActivity";
     private static final String SELECTION_PARAM = "SELECTION";
-    private static final String SELECION_ARG_PARAM = "SELECTION_ARGS";
+    private static final String SELECTION_ARG_PARAM = "SELECTION_ARGS";
     private static final String SORT_ORDER_PARAM = "SORT_ORDER";
 
     private Bundle args = new Bundle();
@@ -79,7 +85,7 @@ public class DurationsReportActivity extends AppCompatActivity implements Loader
                 getSupportLoaderManager().restartLoader(LOADER_ID, args, this);
                 return true;
             case R.id.rm_filter_date:
-                //TODO showDatePickerDialog();
+                showDatePickerDialog("Select date for report", DIALOG_FILTER);
                 return true;
             case R.id.rm_delete:
                 //TODO showDatePickerDialog(); //Actucal Deleting will be done in onDataSer();
@@ -103,18 +109,47 @@ public class DurationsReportActivity extends AppCompatActivity implements Loader
         return super.onPrepareOptionsMenu(menu);
     }
 
+    private void showDatePickerDialog(String title, int dialogId) {
+        Log.d(TAG, "showDatePickerDialog: starts");
+        DialogFragment dialogFragment = new PickedDateFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(PickedDateFragment.DATE_PICKER_ID, dialogId);
+        arguments.putString(PickedDateFragment.DATE_PICKER_TITLE, title);
+        arguments.putSerializable(PickedDateFragment.DATE_PICKER_DATE, calendar.getTime());
+        dialogFragment.setArguments(arguments);
+        dialogFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Log.d(TAG, "onDateSet: called");
+        //Check the id
+        int dialogId = (int) view.getTag();
+        switch (dialogId) {
+            case DIALOG_FILTER:
+                calendar.set(year, month, dayOfMonth, 0, 0, 0);
+                applyFilter();
+                getSupportLoaderManager().restartLoader(LOADER_ID, args, this);
+                break;
+            case DIALOG_DELETE:
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid mode when receiving DataPickerDialog result!");
+        }
+    }
+
     private void applyFilter() {
         Log.d(TAG, "applyFilter: starts");
 
         if (displayWeek) {
-          //Shows entries by week.
+            //Shows entries by week.
 
-            Date currentDate= calendar.getTime();// saves current date
+            Date currentDate = calendar.getTime();// saves current date
             int dayOfWeek = calendar.get(GregorianCalendar.DAY_OF_WEEK);
             int weekStart = calendar.getFirstDayOfWeek();
-            Log.d(TAG, "applyFilter: First day of the week: "+ weekStart);
-            Log.d(TAG, "applyFilter: Day of the week: "+ dayOfWeek);
-            Log.d(TAG, "applyFilter: Date is: "+ calendar.getTime());
+            Log.d(TAG, "applyFilter: First day of the week: " + weekStart);
+            Log.d(TAG, "applyFilter: Day of the week: " + dayOfWeek);
+            Log.d(TAG, "applyFilter: Date is: " + calendar.getTime());
             //calculate week start and end dates.
             calendar.set(GregorianCalendar.DAY_OF_WEEK, weekStart);
             String startDate = String.format(Locale.US, "%04d-%02d-%02d",
@@ -122,19 +157,19 @@ public class DurationsReportActivity extends AppCompatActivity implements Loader
                     calendar.get(GregorianCalendar.MONTH) + 1,
                     calendar.get(GregorianCalendar.DAY_OF_MONTH));
 
-            calendar.add(GregorianCalendar.DATE,6); // move forward 6 days to get the last day of the week.
+            calendar.add(GregorianCalendar.DATE, 6); // move forward 6 days to get the last day of the week.
 
             String endDate = String.format(Locale.US, "%04d-%02d-%02d",
                     calendar.get(GregorianCalendar.YEAR),
                     calendar.get(GregorianCalendar.MONTH) + 1,
                     calendar.get(GregorianCalendar.DAY_OF_MONTH));
-            String[] selectionArgs = new String[]{startDate,endDate};
+            String[] selectionArgs = new String[]{startDate, endDate};
 
             //put calendar back to where it was before we started jumping back and forth.
             calendar.setTime(currentDate);
-            Log.d(TAG, "applyFilter: Start Date is: "+startDate+ "End date: "+ endDate );
-            args.putString(SELECTION_PARAM,"StartDate Between ? AND ?");
-            args.putStringArray(SELECION_ARG_PARAM,selectionArgs);
+            Log.d(TAG, "applyFilter: Start Date is: " + startDate + "End date: " + endDate);
+            args.putString(SELECTION_PARAM, "StartDate Between ? AND ?");
+            args.putStringArray(SELECTION_ARG_PARAM, selectionArgs);
 
         } else {
             //day entries.
@@ -142,10 +177,10 @@ public class DurationsReportActivity extends AppCompatActivity implements Loader
                     calendar.get(GregorianCalendar.YEAR),
                     calendar.get(GregorianCalendar.MONTH) + 1,
                     calendar.get(GregorianCalendar.DAY_OF_MONTH));
-            String[] selectionArgs= new String[]{startDate};
-            Log.d(TAG, "applyFilter: Start date: "+startDate );
-            args.putString(SELECTION_PARAM,"StartDate= ?");
-            args.putStringArray(SELECION_ARG_PARAM,selectionArgs);
+            String[] selectionArgs = new String[]{startDate};
+            Log.d(TAG, "applyFilter: Start date: " + startDate);
+            args.putString(SELECTION_PARAM, "StartDate= ?");
+            args.putStringArray(SELECTION_ARG_PARAM, selectionArgs);
         }
     }
 
@@ -165,7 +200,7 @@ public class DurationsReportActivity extends AppCompatActivity implements Loader
 
                 if (args != null) {
                     selection = args.getString(SELECTION_PARAM);
-                    selectionArgs = args.getStringArray(SELECION_ARG_PARAM);
+                    selectionArgs = args.getStringArray(SELECTION_ARG_PARAM);
                     sortOrder = args.getString(SORT_ORDER_PARAM);
                 }
                 return new CursorLoader(this, DurationsContract.CONTENT_URI,
